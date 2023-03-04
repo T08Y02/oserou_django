7de71a7fa235cb58bbc51d637bd2ea588309857c
T08Y02 <pon9q9@gmail.com>
2023-03-04 13:02:36 +0900

    colorが設定されない

--- oserou/oserou_forplay/static/js/ban.js
+++ oserou/oserou_forplay/static/js/ban.js
@@ -1,3 +1,20 @@
+/*
+0 = blank
+1 = black
+2 = white 
+*/
+
+function buttonClick(e){
+  alert('Click' + this.row +  this.collumn);
+  alert(e.target.style.background-color)
+  //stoneColorChange("black")
+}
+
+function stoneColorChange(e){
+  let stone = e.target;
+  stone.style.color = new_color;
+}
+
 function init(){
   let board = document.getElementById("board");
 
@@ -6,10 +23,11 @@ function init(){
     for(let d = 0; d < 8; d++) {
       let td = document.createElement("td");
       td.className = "cell";
-      td.setAttribute("id", "cell"+ (7 - d) + r);
+      td.setAttribute("id", "cell"+ d + r);
       let ts = document.createElement("td");
       ts.className = "stone";
-      ts.setAttribute("id", "stone"+ (7 - d) + r);
+      ts.setAttribute("id", "stone"+ d + r);
+      ts.addEventListener('click', {collumn:d, row:r, handleEvent:buttonClick});
       td.appendChild(ts);
       tr.appendChild(td);
     }

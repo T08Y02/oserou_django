12a1ffa8a2ef8cce874cebd319af862e5eab43f8
T08Y02 <pon9q9@gmail.com>
2023-03-04 22:48:13 +0900

    board_stateの受け渡し実装

--- oserou/oserou_forplay/static/js/ban.js
+++ oserou/oserou_forplay/static/js/ban.js
@@ -14,15 +14,31 @@ function stoneColorChange(row, collumn, new_color){
   stone.style.backgroundColor = new_color;
 }
 
-function refreshBoard(boardState){
-
+function refreshBoard(board_state){
   for(let r = 0; r < 8; r++) {
     for(let c = 0; c < 8; c++) {
       let dic = ["gray", "black", "white"];
-      let new_color = dic[boardState[r][c]];
+      let new_color = dic[board_state[r][c]];
       stoneColorChange(r, c, new_color);
     }
   }
+}
+
+function initBoard(){
+  let board_state = new Array(8);
+  for(let i = 0; i < 8; i++) {
+      if (i == 3){
+        board_state[i] = [0, 0, 0, 2, 1, 0, 0, 0];
+      }
+      else if(i == 4){
+        board_state[i] = [0, 0, 0, 1, 2, 0, 0, 0];
+      }
+      else{
+        board_state[i] = new Array(8).fill(0);
+      }
+    }
+    alert(board_state);
+    refreshBoard(board_state);
 }
 
 function init(){
@@ -42,20 +58,6 @@ function init(){
     }
     board.appendChild(tr);
   }
-
-  //init board
-  let boardState = new Array(8);
-  for(let i = 0; i < 8; i++) {
-      if (i == 3){
-        boardState[i] = [0, 0, 0, 2, 1, 0, 0, 0];
-      }
-      else if(i == 4){
-        boardState[i] = [0, 0, 0, 1, 2, 0, 0, 0];
-      }
-      else{
-        boardState[i] = new Array(8).fill(0);
-      }
-    }
-    alert(boardState);
-    refreshBoard(boardState);
+  alert(board_state);
+  refreshBoard(board_state);
 }
--- oserou/oserou_forplay/urls.py
+++ oserou/oserou_forplay/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ban', views.ban, name='ban'),
+    path('board', views.board, name='board'),
 ]
